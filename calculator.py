ffcd3bad4dfc57ac7cf2295f74109bd35abf314b
Lg-123-abc <lagroudas@gmail.com>
2025-11-16 14:48:23 -0500

    merged changes

--- calculator.py
+++ calculator.py
@@ -1,22 +1,11 @@
 import math
 
-def square_root(a):
-    try:
-        result = math.sqrt(a)
-        return result
-    except ValueError as e:
-        print("Value Error")
-        return None
-
-def hypotenuse(a,b):
-    return math.hypot(a, b)
-
 # First example
 def add(a, b):
     return a + b
 
-def sub(a,b)
-    return a-b
+def subtract(a, b):
+    return a - b
 
 def mul(a, b):
     return a * b
@@ -27,23 +16,19 @@ def div(a, b):
     else:
         return b / a
 
-def log(a, b):
-    if b <= 0 or a <= 0 or a == 1:
-        raise ValueError()
-    else:
-        return math.log(b, a)
+def logarithm(a, b): # use math library/raise ValueError
+    return math.log(a,b)
 
 def exp(a, b):
     return math.pow(a, b)
 
-def subtract(a, b):
-    return a - b
+def square_root(a):
+    try:
+        result = math.sqrt(a)
+        return result
+    except ValueError as e:
+        print("Value Error")
+        return None
 
-def multiply(a, b):
-    return a * b
-
-def logarithm(a, b): # use math library/raise ValueError
-    return math.log(a,b)
-
-def exponent(a, b):
-    return a ** b
+def hypotenuse(a,b):
+    return math.hypot(a, b)
